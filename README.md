8a4401b919f841deb1bb3f265d279fff022dc45e
Henning Kockerbeck <henning@henning-kockerbeck.de>
2018-08-15 12:37:24 +0000

    Polish things up to be publication ready
    Add readme and licence information, add some comments to init.php.

---
+++ README.md
@@ -0,0 +1,26 @@
+# ttrss-tumblr-gdpr-ua
+
+Plugin for the RSS Reader (Tiny Tiny RSS)[https://tt-rss.org/] to handle RSS feeds from Tumblr in Europe.
+
+# What does it do?
+
+Because of European laws for the protection of data privacy ((GDPR)[https://en.wikipedia.org/wiki/General_Data_Protection_Regulation]), Tumblr's parent company Oath doesn't directly deliver its contents to users in Europe. Instead, the users are redirected to a page where they're asked to give Oath permission to handle, process etc. their data. Once that permission is given, the users can access the content they came for. For future visits, the permission is stored in a cookie.
+
+Obviously, an automated system like TT-RSS gets tripped up if it can't just get a feed from an url like normal, but has to jump through some hoops instead. A few months ago, GregThib published [a plugin](https://github.com/GregThib/ttrss-tumblr-gdpr) that basically provided Oath with the permission cookie it expected, so TT-RSS could get to the feed.
+
+But a short while ago, the plugin stopped working. It looks like Oath changed some detail of the permission process somewhere, so the cookie provided by Greg's plugin doesn't work anymore. Instead of delving into the murky details of the plugin (and repeating that every time Oath modifies its process), this plugin uses another "trick": If Oath thinks the request comes not from a "real" user, but a search engine's crawler, it doesn't bother with the permission page and delivers the content directly. Hopefully, they will continue to do so in the long run.
+
+So this plugin checks whether the feed that TT-RSS needs to handle is from _tumblr.com_ or a subdomain of it. If it is, the plugin switches the user agent to `googlebot`. Additionally to _tumbler.com_, you can add other domains that are hosted by Tumblr in the preferences.
+
+# How to install?
+
+-   Download the plugin and put it into the `plugins.local` directory of your TT-RSS installation. Alternatively, you can put it into the `plugins` directory. The directory containing the plugin _must_ be named `tumblr_gdpr_ua`.
+-   Activate the plugin in TT-RSS' settings.
+-   In case you want to subscribe to a feed that's hosted by Tumblr, but at a domain _other_ than _tumblr.com_ or its subdomains, add that domain to the plugin's setting's.
+
+# Thanks
+
+Big thanks to
+
+-   [GregThib](https://github.com/GregThib) for writing the original plugin. This plugin is, you may say, largely inspired by his.
+-   [homlett](https://discourse.tt-rss.org/t/change-on-tumblr-rss-feeds-not-working/1158/96) for pointing out that the right user agent make Oath deliver the goods directly.
--- init.php
+++ init.php
@@ -2,12 +2,11 @@
 class Tumblr_GDPR_UA extends Plugin
 {
     private $host;
-    private $tumblr_domains = array();
 
     public function about()
     {
         return array(
-          0.01,
+          1.0,
           "Fixes Tumblr feeds for GDPR compliance by masquerading as GoogleBot
           (changing user agent). Requires curl.",
           "hkockerbeck");
@@ -25,7 +24,9 @@ class Tumblr_GDPR_UA extends Plugin
 
     public function init($host)
     {
+        // store the provided reference to host
         $this->host = $host;
+        // hook on some hooks ;)
         if (function_exists("curl_init")) {
             $host->add_hook($host::HOOK_SUBSCRIBE_FEED, $this);
             $host->add_hook($host::HOOK_FEED_BASIC_INFO, $this);
@@ -34,19 +35,23 @@ class Tumblr_GDPR_UA extends Plugin
         }
     }
 
+    // when subscribing to a new feed
     public function hook_subscribe_feed(
       $feed_data,
       $fetch_url,
       $auth_login,
       $auth_pass
     ) {
+        // if the feed is hosted by Tumblr
         if ($this->is_tumblr_domain($fetch_url)) {
+            // re-fetch the feed data with changed user agent
             $feed_data = $this->fetch_contents($fetch_url, $auth_login, $auth_pass);
         }
 
         return $feed_data;
     }
 
+    // get basic info about a feed (title and site url, mostly)
     public function hook_feed_basic_info(
       $basic_info,
       $fetch_url,
@@ -55,9 +60,12 @@ class Tumblr_GDPR_UA extends Plugin
       $auth_login,
       $auth_pass
     ) {
+        // if the feed is hosted by Tumblr
         if ($this->is_tumblr_domain($fetch_url)) {
+            // re-fetch the feed data with changed user agent
             $contents = $this->fetch_contents($fetch_url, $auth_login, $auth_pass);
 
+            // extract info we need from the feed data
             $parser = new FeedParser($contents);
             $parser->init();
             if (!$parser->error()) {
@@ -71,6 +79,7 @@ class Tumblr_GDPR_UA extends Plugin
         return $basic_info;
     }
 
+    // fetch feed to look for new articles
     public function hook_fetch_feed(
       $feed_data,
       $fetch_url,
@@ -80,28 +89,35 @@ class Tumblr_GDPR_UA extends Plugin
       $auth_login,
       $auth_pass
     ) {
+        // if the feed is hosted by Tumblr
         if ($this->is_tumblr_domain($fetch_url)) {
+            // re-fetch the feed data with changed user agent
             $feed_data = $this->fetch_contents($fetch_url, $auth_login, $auth_pass);
         }
         return $feed_data;
     }
 
+    // segment in TT-RSS' prefs to add additional domains
     public function hook_prefs_tab($args)
     {
         if ($args != "prefPrefs") {
             return;
         }
 
+        // replacements in the template
         $replacements = array(
           '{title}' => 'Tumblr GDPR UA',
           '{content}' => implode(PHP_EOL, $this->host->get($this, 'tumblr_domains', array())). PHP_EOL
       );
 
+        // set up a _very_ basic template engine
+        // so we don't have print out everything
         $template = file_get_contents(__DIR__."/pref_template.html");
         $template = str_replace(array_keys($replacements), array_values($replacements), $template);
         print $template;
     }
 
+    // save data from prefs segment
     public function save()
     {
         $tumblr_domains = explode("\r\n", $_POST['tumblr_domains']);
@@ -109,6 +125,7 @@ class Tumblr_GDPR_UA extends Plugin
         $this->host->set($this, 'tumblr_domains', $tumblr_domains);
     }
 
+    // fetch feed data with changed user agent
     private function fetch_contents(
       $fetch_url,
       $auth_login = false,
@@ -122,17 +139,22 @@ class Tumblr_GDPR_UA extends Plugin
         return fetch_file_contents($options);
     }
 
+    // helper function: does string $haystack end with string $needle?
     private function ends_with($haystack, $needle)
     {
         return mb_substr($haystack, -mb_strlen($needle)) === $needle;
     }
 
+    // is the domain in question on tumblr.com or one of the additional domains?
     private function is_tumblr_domain($fetch_url)
     {
+        // extract domain from whole url
         $url = parse_url($fetch_url, PHP_URL_HOST);
+        // look through list of "known tumblr" urls
         $domains = $this->host->get($this, 'tumblr_domains', array());
         array_push($domains, 'tumblr.com');
         $found = array_filter($domains, function ($t) use ($url) {
+            // does the domain in question end with a tumblr url?
             return $this->ends_with($url, $t);
         });
 
